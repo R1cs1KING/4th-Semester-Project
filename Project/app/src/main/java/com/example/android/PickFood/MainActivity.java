0d25d26e0c2e8a0bdcfcfd5799430069fe706600
R1cs1KING <ricsi960413@gmail.com>
2017-03-31 16:15:11 +0200

    commit 7
    Login and logout are working

--- Project/app/src/main/java/com/example/android/PickFood/MainActivity.java
+++ Project/app/src/main/java/com/example/android/PickFood/MainActivity.java
@@ -1,11 +1,15 @@
 
 package com.example.android.PickFood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,5 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
        getMenuInflater().inflate(R.menu.action_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //handle presses on the action bar items
+        switch (item.getItemId()) {
+
+            case R.id.signout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LogIn.class));
+                finish();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
