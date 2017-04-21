1cd9540e68d53ea9b630d6d144b478aa3554ca11
R1cs1KING <ricsi960413@gmail.com>
2017-04-21 17:17:58 +0200

    added camera and barcode

---
+++ Meter Replacement/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace Meter_Replacement
+{
+    [Activity(Label = "Meter_Replacement", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var btn = FindViewById(Resource.Id.button1);
+            // SetContentView (Resource.Layout.Main);
+
+            /*btn.Click += (s, e) => {
+                Intent nextActivity = new Intent(this, typeof(MenuActivity));
+                StartActivity(nextActivity);
+            };*/
+            btn.Click += delegate {
+                StartActivity(typeof(MenuActivity));
+            };
+        }
+    }
+}
+
---
+++ Meter Replacement/MenuActivity.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ZXing.Mobile;
+
+namespace Meter_Replacement
+{
+    [Activity(Label = "Menu", Icon = "@drawable/icon")]
+    class MenuActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Menu);
+            var btn = FindViewById(Resource.Id.button1);
+            var btnCamera = FindViewById(Resource.Id.button2);
+            var buttonScan = FindViewById(Resource.Id.button3);
+            // SetContentView (Resource.Layout.Main);
+            MobileBarcodeScanner.Initialize(Application);
+            btn.Click += (s, e) => {
+                Intent nextActivity = new Intent(this, typeof(ReplacementDataActivity));
+                StartActivity(nextActivity);
+            };
+            btnCamera.Click += delegate {
+                StartActivity(typeof(CameraActivity));
+            };
+            buttonScan.Click += async (sender, e) =>
+            {
+
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                var result = await scanner.Scan();
+
+                if (result != null)
+                    Console.WriteLine("Scanned Barcode: " + result.Text);
+            };
+        }
+    }
+}
