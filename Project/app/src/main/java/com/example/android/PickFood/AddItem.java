a48d2791990a1da8094a70d2a8419d86e076e333
R1cs1KING <richardtarnoczi@Richards-MacBook-Air.local>
2017-05-06 12:10:46 +0200

    take picture
    pictures can be taken and stored in a folder on the phone called
    PickFood

--- Project/app/src/main/java/com/example/android/PickFood/AddItem.java
+++ Project/app/src/main/java/com/example/android/PickFood/AddItem.java
@@ -1,9 +1,15 @@
 package com.example.android.PickFood;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,10 +19,14 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.TextView;
+import android.Manifest;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,11 +34,12 @@ import java.util.Map;
  * Created by R1cs1 on 2017.04.04..
  */
 
-public class AddItem extends AppCompatActivity{
+public class AddItem extends Activity{
 
     private EditText mName, mLocation, mDescription;
     private ImageView mPicture;
     private Button proceed, cancel, uploadPic;
+    Uri file;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mRef = database.getReferenceFromUrl("https://pickfood-5c351.firebaseio.com/");
 
@@ -46,6 +57,11 @@ public class AddItem extends AppCompatActivity{
         proceed = (Button) findViewById(R.id.btn_proceed);
         cancel = (Button) findViewById(R.id.btn_cancel);
         uploadPic = (Button) findViewById(R.id.btn_upload);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            uploadPic.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
 
         final Spinner spinner = (Spinner) findViewById(R.id.category_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -79,28 +95,57 @@ public class AddItem extends AppCompatActivity{
             }
         });
 
-        uploadPic.setOnClickListener(new View.OnClickListener()  {
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+        uploadPic.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                file = Uri.fromFile(getOutputMediaFile());
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+                startActivityForResult(intent, 100);
             }
         });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                uploadPic.setEnabled(true);
+            }
+        }
+    }
 
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                mPicture.setImageURI(file);
+            }
+        }
+    }
 
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "PickFood");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
     }
 
     private void toastMessage(String s) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        mPicture.setImageBitmap(bitmap);
     }
 }
